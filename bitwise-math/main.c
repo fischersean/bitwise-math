339dd78039319872dcb1255886b6354584839946
hapins <seanieboy10@gmail.com>
2019-05-18 12:59:48 -0400

    Initial Commit

---
+++ bitwise-math/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  bitwise-math
+//
+//  Created by Sean Fischer on 5/18/19.
+//  Copyright © 2019 Sean Fischer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
